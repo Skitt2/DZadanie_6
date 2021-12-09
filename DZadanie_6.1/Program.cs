a7f098cb3dc34a3238b2e9352ac4457b6e0eef92
Skitt2 <provaid@yandex.ru>
2021-12-09 13:08:30 +0300

    Добавьте файлы проекта.

---
+++ DZadanie_6.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZadanie_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются. Найти самое длинное слово в строке.");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int lengthWord;
+            int maxLength = 0;
+            string longWord = "";
+            foreach (string s in stringArray)
+            {
+                lengthWord = s.Length;
+                if (lengthWord > maxLength)
+                {
+                    maxLength = lengthWord;
+                    longWord = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении \"{0}\" ", longWord);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZadanie_6.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZadanie_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string resultString = "";
+            string reverseString = "";
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                resultString += stringArray[i];
+            }
+            //Console.WriteLine(resultString);
+            for (int j = resultString.Length - 1; j >= 0; j--)
+            {
+                reverseString += resultString[j];
+            }
+            //Console.WriteLine(reverseString);
+            if (String.Compare(resultString, reverseString, true) == 0)
+            {
+                Console.WriteLine("Cтрока является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Cтрока не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
